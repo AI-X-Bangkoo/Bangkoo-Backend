e44f8a7819ad750134a204e2bfdc711cc6bd416b
nicdkim <zzsehdrb@gmal.com>
2025-04-26 11:43:52 +0900

    search 미사용 파일 정리

--- src/main/java/com/bangkoo/back/config/SecurityConfig.java
+++ src/main/java/com/bangkoo/back/config/SecurityConfig.java
@@ -98,7 +98,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://bangkoo.store", "https://www.bangkoo.store", "https://api.bangkoo.store"));
+//        configuration.setAllowedOrigins(Arrays.asList("https://bangkoo.store", "https://www.bangkoo.store", "https://api.bangkoo.store"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // HttpOnly 쿠키 사용 시 필요
